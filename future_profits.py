47d78a4cd139f842a7b356b4c0beea75e3aea8ff
Quinn <qhawkins2002@gmail.com>
2024-02-09 19:30:19 -0500

    subtracted initial basis from calculation for future profits

--- future_profits.py
+++ future_profits.py
@@ -10,6 +10,6 @@ def future_profits(prices_v, cash, buffer_len, position, current_tick):
     initial_basis = find_fill_price(prices_v, position, -position, current_tick)+cash
     #initial_basis = (position * self.prices_v[current_tick]) + self.cash
     for i in prange(buffer_len):
-        fut_profit[i] = (find_fill_price(prices_v, position, -position, current_tick + i)+cash)/initial_basis
+        fut_profit[i] = (find_fill_price(prices_v, position, -position, current_tick + i)+cash)-initial_basis/initial_basis
         #5000 * (((position * self.prices_v[current_tick + i]) + self.cash) - initial_basis) / initial_basis
     return fut_profit
