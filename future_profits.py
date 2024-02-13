a95fbaaccd3451aa3d3b61d335fc49f4bf9afa1d
Quinn <qhawkins2002@gmail.com>
2024-02-12 22:43:16 -0500

    working!!!!!!!!!!!

--- future_profits.py
+++ future_profits.py
@@ -11,10 +11,13 @@ def future_profits(prices_v, buffer_len, position, current_tick, action_taken):
     #initial_basis = find_fill_price(prices_v, position, -position, current_tick)+cash
     #initial_basis = (position * self.prices_v[current_tick]) + self.cash
     for i in prange(buffer_len):
-        with_trade = execute_trade(prices_v, position+action_taken, current_tick + i)
-        wo_trade = execute_trade(prices_v, position, current_tick + i)
-        fut_profit[i] = with_trade/wo_trade
-        print(f'with_trade: {with_trade}, wo_trade: {wo_trade}, fut_profit: {fut_profit[i]}')
+        with_trade = execute_trade(prices_v, -(position+action_taken), current_tick + i)
+        wo_trade = execute_trade(prices_v, -position, current_tick + i)
+        print(f'with_trade: {with_trade}, wo_trade: {wo_trade}')
+        if wo_trade != 0:
+            fut_profit[i] = with_trade/wo_trade
+        else:
+            fut_profit[i] = 0
         #fut_profit[i] = ((find_fill_price(prices_v, position, -position, current_tick + i)+cash)-initial_basis)/initial_basis
         #5000 * (((position * self.prices_v[current_tick + i]) + self.cash) - initial_basis) / initial_basis
     return fut_profit
